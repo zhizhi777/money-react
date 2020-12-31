e9bd7b0c85b5af7128373d4a5243360985ea900e
zhizhi777 <3375342132@qq.com>
2020-12-31 17:51:48 +0800

    完成 nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require('../icons/tag.svg');
+require('../icons/money.svg');
+require('../icons/chart.svg');
+
+
 
 const Nav =  styled.nav`
   line-height: 24px;
@@ -9,9 +14,16 @@ const Nav =  styled.nav`
     display: flex;
     >li{
       width: 33.33333%;
-      padding: 10px;
+      padding: 4px 0;
       text-align: center;
-      
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `
@@ -21,12 +33,21 @@ const Component = ()=>{
         <Nav>
             <ul>
                 <li>
-                    <Link to="/">记账</Link>
-                </li>
-                <li>
+                    <svg className="icon">
+                        <use xlinkHref='#tag'></use>
+                    </svg>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref='#money'></use>
+                    </svg>
+                    <Link to="/">记账</Link>
+                </li>
+                <li>
+                    <svg className="icon">
+                        <use xlinkHref='#chart'></use>
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
