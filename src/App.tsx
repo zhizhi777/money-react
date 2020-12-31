49e441616d11be0c323558f97b22fc9235931060
zhizhi777 <3375342132@qq.com>
2020-12-31 17:24:42 +0800

    Default Changelist

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import styled from "styled-components";
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Wrapper>
+
+          <Main>
+            <Switch>
+              <Route path="/tags">
+                <Tags />
+              </Route>
+              <Route path="/statistics">
+                <Statistics />
+              </Route>
+              <Route path="/">
+                <Money />
+              </Route>
+            </Switch>
+          </Main>
+
+          <Nav />
+
+        </Wrapper>
+      </Router>
   );
+}
+
+
+function Money() {
+  return <h2>记账</h2>;
+}
+
+function Statistics() {
+  return <h2>统计</h2>;
+}
+
+function Tags() {
+  return <h2>标签</h2>;
 }
 
 export default App;
