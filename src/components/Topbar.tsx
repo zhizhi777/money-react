0e448cd39419495bf062487e7322e9aa26163293
zhizhi777 <3375342132@qq.com>
2021-01-13 19:36:33 +0800

    提出公共代码

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  line-height: 20px;
+  padding: 14px;
+  background-color: rgb(249,219,97);
+`
+
+export {Topbar}
